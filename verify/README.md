bf26f83b99be2427ed968a4802ad8c3fbd301992
pengluhyd <lup@vmware.com>
2019-04-30 16:49:21 +0800

    Use const and let instead of var in javascript
    Update /verify/README.md
    
    Signed-off-by: pengluhyd <lup@vmware.com>

--- verify/README.md
+++ verify/README.md
@@ -9,8 +9,28 @@ npm install -g truffle-flattener
 Flatten contracts
 
 ```
+mkdir truffle-flattener
 truffle-flattener contracts/OrdersV1.sol > truffle-flattener/OrdersV1Flattened.sol
 truffle-flattener contracts/OrdersProxy.sol > truffle-flattener/OrdersProxyFlattened.sol
+```
+
+Update the configuration in verify.js with the correct host and basic authentication username and password.
+
+```
+  const username = '<username>';
+  const password = '<password>';
+  const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+
+  const options = {
+    host: "localhost",
+    port: 8080,
+    path: "/api/concord/contracts/" + address,
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": auth
+    }
+  };
 ```
 
 Verify and upload contracts to Helen
--- verify/verify.js
+++ verify/verify.js
@@ -1,16 +1,16 @@
-var fs = require("fs");
-var http = require("http");
+const fs = require("fs");
+const http = require("http");
 
 verifyMigrations();
 verifyOrdersV1();
 verifyOrdersProxy()
 
 function verify(data, address) {
-  var username = '<username>';
-  var password = '<password>';
-  var auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
+  const username = '<username>';
+  const password = '<password>';
+  const auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
 
-  var options = {
+  const options = {
     host: "localhost",
     port: 8080,
     path: "/api/concord/contracts/" + address,
@@ -23,7 +23,6 @@ function verify(data, address) {
 
   const req = http.request(options, (res) => {
     console.log(`statusCode: ${res.statusCode}`);
-    console.log(`statusCode: ${res}`);
   });
 
   req.on("error", (error) => {
@@ -35,12 +34,12 @@ function verify(data, address) {
 }
 
 function verifyMigrations() {
-  var content = fs.readFileSync("./build/contracts/Migrations.json");
-  var sourcecode = fs.readFileSync("./contracts/Migrations.sol").toString();
-  var jsonContent = JSON.parse(content);
-  var address = jsonContent.networks[5000].address;
+  const content = fs.readFileSync("./build/contracts/Migrations.json");
+  const sourcecode = fs.readFileSync("./contracts/Migrations.sol").toString();
+  const jsonContent = JSON.parse(content);
+  const address = jsonContent.networks[5000].address;
 
-  var data = JSON.stringify({
+  const data = JSON.stringify({
     "contract_id": "Migrations",
     "compiler_version": "v0.4.25+commit.59dbf8f1",
     "sourcecode": sourcecode,
@@ -52,14 +51,14 @@ function verifyMigrations() {
 }
 
 function verifyOrdersV1() {
-  var content = fs.readFileSync("./build/contracts/OrdersV1.json");
+  const content = fs.readFileSync("./build/contracts/OrdersV1.json");
   // Generate this file with the command
   // "truffle-flattener contracts/OrdersV1.sol > truffle-flattener/OrdersV1Flattened.sol"
-  var sourcecode = fs.readFileSync("./truffle-flattened/OrdersV1Flattened.sol").toString();
-  var jsonContent = JSON.parse(content);
-  var address = jsonContent.networks[5000].address;
+  const sourcecode = fs.readFileSync("./truffle-flattener/OrdersV1Flattened.sol").toString();
+  const jsonContent = JSON.parse(content);
+  const address = jsonContent.networks[5000].address;
 
-  var data = JSON.stringify({
+  const data = JSON.stringify({
     "contract_id": "OrdersV1",
     "compiler_version": "v0.4.25+commit.59dbf8f1",
     "sourcecode": sourcecode,
@@ -71,14 +70,14 @@ function verifyOrdersV1() {
 }
 
 function verifyOrdersProxy() {
-  var content = fs.readFileSync("./build/contracts/OrdersProxy.json");
+  const content = fs.readFileSync("./build/contracts/OrdersProxy.json");
   // Generate this file with the command
   // "truffle-flattener contracts/OrdersProxy.sol > truffle-flattener/OrdersProxyFlattened.sol"
-  var sourcecode = fs.readFileSync("./truffle-flattened/OrdersProxyFlattened.sol").toString();
-  var jsonContent = JSON.parse(content);
-  var address = jsonContent.networks[5000].address;
+  const sourcecode = fs.readFileSync("./truffle-flattener/OrdersProxyFlattened.sol").toString();
+  const jsonContent = JSON.parse(content);
+  const address = jsonContent.networks[5000].address;
 
-  var data = JSON.stringify({
+  const data = JSON.stringify({
     "contract_id": "OrdersProxy",
     "compiler_version": "v0.4.25+commit.59dbf8f1",
     "sourcecode": sourcecode,
