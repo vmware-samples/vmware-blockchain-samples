0d60877941237531a5061478a38d7ab6d5dd6226
Ubuntu <ubuntu@ip-10-0-0-202.us-west-1.compute.internal>
2021-02-28 22:15:36 +0000

    cleaned up version for parallelism

--- web3py/lrt-examples/eth.py
+++ web3py/lrt-examples/eth.py
@@ -47,12 +47,23 @@ compiled_sol = compile_standard({
 account1 = '0xf17f52151EbEF6C7334FAD080c5704D77216b732'
 key1 = '0xae6ae8e5ccbfb04590405997ee2d52d2b330726137b875053c36d94e974d162f'
 
+vmware = 0
+chainid = 1
+total_tx_count = 100
+
 # web3.py instance
 #w3 = Web3(Web3.EthereumTesterProvider())
-w3 = Web3(Web3.HTTPProvider("http://54.160.229.176:8545"))
-#w3 = Web3(Web3.HTTPProvider('http://54.219.187.21:32000')) #besu single cluster
-#w3 = Web3(Web3.HTTPProvider('http://54.241.133.23:32000')) #besu multi cluster
-#w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+if (vmware == 1):
+  w3 = Web3(Web3.HTTPProvider("http://54.160.229.176:8545"))
+  chainid = 1
+else:
+  print("besu .............................")
+  w3 = Web3(Web3.HTTPProvider('http://10.0.0.65:32000')) #besu single cluster
+  #w3 = Web3(Web3.HTTPProvider('http://54.219.187.21:32000')) #besu single cluster
+  #w3 = Web3(Web3.HTTPProvider('http://54.241.133.23:32000')) #besu multi cluster
+  chainid = 2018
+
+w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 
 # get bytecode
 bytecode = compiled_sol['contracts']['Greeter.sol']['Greeter']['evm']['bytecode']['object']
@@ -66,9 +77,10 @@ Greeter = w3.eth.contract(abi=abi, bytecode=bytecode)
 construct_txn = Greeter.constructor().buildTransaction({
     'from': account1,
     'gas': 2000000,
-    'gasPrice': 234567897654321,
+    #'gasPrice': 234567897654321,
+    'gasPrice': 0,
     'nonce': w3.eth.getTransactionCount(account1),
-    'chainId': 1
+    'chainId': chainid
 })
 signed_txn = w3.eth.account.sign_transaction(construct_txn, key1)
 tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
@@ -86,26 +98,42 @@ greeter = w3.eth.contract(
 )
 print(greeter.functions.greet().call())
 
+start_time = time.time()
 count1 = 0
-count = 10000
-while (count > 0):
+txlist = []
+nonce = w3.eth.getTransactionCount(account1)
+while (count1 < total_tx_count):
     construct_txn = greeter.functions.setGreeting('Nihao').buildTransaction({
         'from': account1,
-        'gas': 2000000,
-        'gasPrice': 234567897654321,
-        'nonce': w3.eth.getTransactionCount(account1),
-        'chainId': 1
+        #'gas': 2000000,
+        'gas':  200000,
+        #'gas': 0x1ffffffffffffe,
+        #'gasPrice': 234567897654321,
+        'gasPrice': 0,
+        'nonce': nonce,
+        'chainId': chainid
     })
     signed_txn = w3.eth.account.sign_transaction(construct_txn, key1)
     tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
-    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
-    print(tx_receipt)
+    txlist.append(tx_hash)
+    count1 += 1
+    nonce += 1
+    #time.sleep(1)
+
+count2 = 0
+while (count2 < count1):
+    tx_receipt = w3.eth.waitForTransactionReceipt(txlist[count2])
+    #print(tx_receipt)
     if (tx_receipt.status == 1):
-        print("tx success, complete:remaining",count1,":",count)
-        print(greeter.functions.greet().call())
+        if (count1-count2-1 == 0):
+            break
+        #print(greeter.functions.greet().call())
     else:
         print("tx failed")
         quit()
-    count1 += 1
-    count -= 1
-    time.sleep(1)
+    count2 += 1
+    #time.sleep(1)
+end_time = time.time()
+print("--- %s seconds ---" % (end_time - start_time))
+print("tx success, complete:remaining",total_tx_count,":",0)
+print("TPS:", total_tx_count/(end_time - start_time))
