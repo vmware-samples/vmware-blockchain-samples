22eb1f8f4d88bc014e22d6deaa7c0c76391af957
ramki krishnan <ramkik@vmware.com>
2021-03-16 22:10:43 -0700

    code to detect hash mismatch

--- web3py/lrt-examples/eth.py
+++ web3py/lrt-examples/eth.py
@@ -12,6 +12,7 @@ from solcx import compile_standard
 from web3.middleware import geth_poa_middleware
 import time
 import urllib3
+from hexbytes import HexBytes
 urllib3.disable_warnings()
 
 # Solidity source code
@@ -66,10 +67,15 @@ total_tx_count = 1000
 if (vmware == 1):
     #w3 = Web3(Web3.HTTPProvider("http://54.160.229.176:8545"))
     if (vmware_prod_deploy == 0):
-        w3 = Web3(Web3.HTTPProvider('https://10.184.110.253:8545', request_kwargs={'verify': False}))
-    chainid = 1
+        #w3 = Web3(Web3.HTTPProvider('https://10.184.110.253:8545', request_kwargs={'verify': False}))
+        print("vmware dev ...")
+        w3 = Web3(Web3.HTTPProvider('http://10.184.110.253:8545'))
+        chainid = 5000
+    else:
+        print("vmware prod ...")
+        w3 = Web3(Web3.HTTPProvider('http://10.40.205.171:8545'))
 else:
-    print("besu .............................")
+    print("besu ...")
     #w3 = Web3(Web3.HTTPProvider('http://10.0.0.65:32000')) #besu single cluster
     w3 = Web3(Web3.HTTPProvider('http://54.241.133.23:32000')) #besu single cluster
     #w3 = Web3(Web3.HTTPProvider('http://54.219.187.21:32000')) #besu single cluster
@@ -97,7 +103,10 @@ construct_txn = Greeter.constructor().buildTransaction({
     'chainId': chainid
 })
 signed_txn = w3.eth.account.sign_transaction(construct_txn, key1)
+tx_hash_send = signed_txn.hash
 tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+if (tx_hash_send != tx_hash):
+    print("hash mismatch")
 tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
 print(tx_receipt)
 if (tx_receipt.status == 1):
@@ -115,6 +124,7 @@ print(greeter.functions.greet().call())
 start_time = time.time()
 count1 = 0
 txlist = []
+txlist_mismatch = []
 nonce = w3.eth.getTransactionCount(account1)
 while (count1 < total_tx_count):
     construct_txn = greeter.functions.setGreeting('Nihao').buildTransaction({
@@ -128,7 +138,17 @@ while (count1 < total_tx_count):
         'chainId': chainid
     })
     signed_txn = w3.eth.account.sign_transaction(construct_txn, key1)
+    tx_hash_send = signed_txn.hash
     tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    if (tx_hash_send != tx_hash):
+        print("hash mismatch",count1+1)
+        #LOG_ERROR(logger, " total_txn_count " << total_txn_count << " Recording transaction hash " << txhash << " nonce " << nonce << " gas_price " << gas_price << " gas_used " << gas_used << " to " << to << " value " << message.value << " data " << message.input_data << " sig_r " << sig_r << " sig_s " << sig_s << " sig_v " << sig_v << " gas_limit " << gas_limit);
+
+        print(" tx_hash ", w3.toHex(tx_hash)," nonce ", nonce, "gas_price ", 0, " gas_used ", 0, " to ", tx_receipt.contractAddress, " r ", w3.toHex(signed_txn.r)," s ", w3.toHex(signed_txn.s)," va ", signed_txn.v," gas_limit ", 200000, " tx_hash_send ", w3.toHex(tx_hash_send)," chainid ", chainid)
+        txlist_mismatch.append(1)
+    else:
+        txlist_mismatch.append(0)
+    txlist.append(tx_hash)
     txlist.append(tx_hash)
     count1 += 1
     nonce += 1
@@ -137,8 +157,9 @@ while (count1 < total_tx_count):
 count2 = 0
 while (count2 < count1):
     tx_receipt = w3.eth.waitForTransactionReceipt(txlist[count2])
-    #print(tx_receipt)
     if (tx_receipt.status == 1):
+        #if (txlist_mismatch[count2] == 1):
+        #    print(tx_receipt)
         if (count1-count2-1 == 0):
             break
         #print(greeter.functions.greet().call())
