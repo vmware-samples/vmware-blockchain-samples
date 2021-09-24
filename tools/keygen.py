c0340b8f76cbec2ce65a3e1e22897151fc8b434f
ramki krishnan <ramkik@vmware.com>
2021-09-24 15:14:06 -0700

    added comment

--- tools/keygen.py
+++ tools/keygen.py
@@ -1,3 +1,4 @@
+# run this script for generating new ethereum account private keys and addresses
 from web3.auto import w3
 acct = w3.eth.account.create('KEYSMASH FJAFJKLDSKF7JKFDJ 1530')
 print(acct.address)
