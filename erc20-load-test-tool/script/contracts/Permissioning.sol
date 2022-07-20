44c3af0f7534e2a13260dae8df0bd57fb8b6fdd3
Nischal Shamra <snischal@vmware.com>
2022-07-20 19:48:40 +0530

    permissioning updated in script

--- erc20-load-test-tool/script/contracts/Permissioning.sol
+++ erc20-load-test-tool/script/contracts/Permissioning.sol
@@ -1,229 +1,147 @@
-pragma solidity ~0.7.6;
+pragma solidity ^0.7.6;
 
 contract Permissioning {
 
-    enum UserStatus {
-        Active,
-        Paused,
-        Inactive
-    }
-
-    // Enums will get converted integers during the compilation so the equivalent value will be 0 - Read, 1 - Write, 2 - Deploy
-    enum UserAction{
+    enum UserAction {
+        None,
         Read,
         Write,
-        Deploy
+        Deploy,
+        Approve
     }
 
-    struct UserRoleData {
-        UserStatus status;
-        bool isActive;
+    // Minimum Admins
+    uint8 constant min_admins = 4;
+
+    // Maximum Admins
+    uint8 constant max_admins = 11;
+
+    // Storage variable will be stored in Blockchain
+    // Use bits to save memory ( uint is 32 bit (0 to 31))
+    // ( N - None, R - Read, W - Write, D - deploy, A - Approve )
+    // 31............8   7   6    5     4     3     2     1    0
+    // -----------------------------------------------------------
+    //  |   |   |   |   |   |   |    |  A  |  D  |  W  |  R |  N  |
+    // -----------------------------------------------------------
+    mapping(address => mapping(address=>uint)) _permissions;
+
+    // User request will be submitted by approver/admin.
+    // Admin should have the key combination (from,to,role)
+    mapping(address => mapping(address => mapping(uint => bool))) _userRequest;
+
+    // Voter should have key combination from,to,role,submitter
+    mapping(address => mapping(address => mapping(uint => mapping(address=>bool)))) _isVoted;
+
+    // approvalCount should have key combination from,to,role
+    mapping(address => mapping(address => mapping(uint => uint8))) _approvalCount;
+
+    // uint8 max value
+    uint8 private _approvalThreshold = 255;
+
+    // events for debugging
+    event VoteUpdateEvent(address indexed _from, address indexed _to, uint8 indexed _role, address  _sender);
+    event NewRequestEvent(address indexed _from, address indexed _to, uint8 indexed _role);
+    event NewApproveEvent(address indexed _from, address indexed _to, uint8 indexed _role);
+
+    // Custom errors - Not supported in 0.7.6 solidity
+    // error RoleAlreadyRequested(address from, address to, uint8 role);
+    // error VotedAlready(address from, address to, uint8 role, address voter);
+
+    // Key: combination of "from" and "to" addressess
+    // Value: set single bit
+    function setPermissionBit(address from, address to, UserAction setRole) internal {
+        _permissions[from][to] |= 1 << uint8(setRole);
     }
 
-    uint8 public  SUPER_ADMIN_ROLE = 1;
-    uint8 public  WRITER_ROLE = 2;
-    uint8 public  READER_ROLE = 3;
-    uint8 public  DEPLOYER_ROLE = 4; // smart-contract deployer
-
-    struct RoleData {
-        mapping(address => UserRoleData) members;
+    // Check if the permission bit for a role is set or not
+    function getPermissionBit(address from, address to, UserAction getRole) internal view returns (bool) {
+        uint tmp = _permissions[from][to];
+        return tmp >> uint8(getRole) & 1 == 1;
     }
 
-    struct VotingData {
-        mapping(address => mapping (uint => bool)) voter;
-    }
-
-    mapping(uint8 => RoleData) private _roles;
-
-    struct NewUserRequest {
-        uint8[] roles;
-        address submitter;
-        uint8 approvalCount;
-    }
-
-    address private owner;
-    mapping(address => NewUserRequest) private newUserRequests;
-
-    event VoteUpdateEvent(address indexed _from, address indexed _userId);
-
-    event NewRequestEvent(address indexed _from, address indexed _userId, uint8[] indexed _roles);
-
-    event NewUserEvent(address indexed _userId, uint8 indexed _role);
-
-    event UserStatusEvent(address indexed _from, address indexed _userId, UserStatus indexed _status);
-
-    mapping (address => VotingData) private approverVotes;
-
-    uint8 private _approvalThreshold = 255; // uint8 max value
-
-    constructor(address[] memory superAdmins) {
-        owner = msg.sender;
-        mapping(address => UserRoleData) storage admins = _roles[SUPER_ADMIN_ROLE]
-            .members;
-        require(superAdmins.length > 3, "Need minimum 4 super admins");
-
-        require(superAdmins.length < 11, "Maximum of 10 super admins are only allowed");
-
-        setApprovalThresholdCount(uint8(superAdmins.length));
-        for (uint8 i = 0; i < superAdmins.length; i++) {
-            admins[superAdmins[i]] = UserRoleData(UserStatus.Active, true);
-        }
-    }
-
-    modifier onlySuperAdmin() {
-        mapping(address => UserRoleData) storage admins = _roles[SUPER_ADMIN_ROLE]
-            .members;
-        require(admins[msg.sender].isActive, "Not a super admin");
-        _;
-    }
-
-     modifier validRole(uint8 roleId) {
-       if(!(roleId == WRITER_ROLE || roleId  == READER_ROLE || roleId  == DEPLOYER_ROLE))
-       {
-           revert("Invalid Role");
-       }
-       _;
-    }
-
-
-    modifier validRoles(uint8[] calldata roles) {
-        if(roles.length == 0)
-        {
-            revert("No Valid Role(s)");
-        }
-        for(uint8 i = 0; i < roles.length; i++){
-            uint8 roleId = roles[i];
-            if(!(roleId == WRITER_ROLE || roleId  == READER_ROLE || roleId  == DEPLOYER_ROLE))
-            {
-                revert("Invalid Role");
-            }
-            _;
-        }
-
-    }
-
-    modifier noExistingRequest(address user) {
-        NewUserRequest storage request =  newUserRequests[user];
-        if(request.submitter != address(0))
-        {
-            revert("There is an existing request for the user in queue");
-        }
-        _;
-    }
-
+    // Set approval threshold
     function setApprovalThresholdCount(uint8 adminCount) private {
-        _approvalThreshold = (adminCount/3)+1;
+        _approvalThreshold = (adminCount/(min_admins-1))+1;
     }
 
+    // Get approval threshold
     function getApprovalThresholdCount() public view returns(uint8){
         return _approvalThreshold;
     }
 
-    function addUser(address newUser, uint8[] calldata roles)
+    // Constructor should accept the addresses from genesis file and make them as approver
+    constructor(address[] memory approvers) {
+
+        require(approvers.length >= min_admins, "Need minimum 4 super admins");
+        require(approvers.length <= max_admins, "Maximum of 10 super admins are only allowed");
+
+        setApprovalThresholdCount(uint8(approvers.length));
+        for (uint8 i = 0; i < approvers.length; i++) {
+            setPermissionBit(approvers[i], address(0x0), UserAction.Approve);
+        }
+    }
+
+    // Avoid multiple requests for an entity
+    modifier noExistingRequest(address _fromAddress, address _toAddress, uint8 _role1) {
+        bool request =  _userRequest[_fromAddress][_toAddress][_role1];
+        if(request)
+        {
+            revert("RoleAlreadyRequested");
+        }
+        _;
+    }
+    // Check the Approve bit has set for the msg.sender
+    modifier onlyApprover() {
+        require(getPermissionBit(msg.sender, address(0x0), UserAction.Approve), "Not an Approver/Admin");
+        _;
+    }
+
+    // Check the request has been approved already
+    modifier notAlreadyApproved(address _from, address _to, uint8 _newrole) {
+        require(! getPermissionBit(_from, _to, UserAction(_newrole)), "Role already authorized");
+        _;
+    }
+
+    // AddUser request from, to, role ( as of now one role at a time )
+    function addUserRequest(address _from, address _to, uint8 _newrole)
         external
-        onlySuperAdmin
-        noExistingRequest(newUser)
-        validRoles(roles)
+        onlyApprover
+        notAlreadyApproved(_from, _to, _newrole)
+        noExistingRequest(_from, _to, _newrole)
     {
-        NewUserRequest memory newRequest =  NewUserRequest({
-            roles: roles,
-            submitter: msg.sender,
-            approvalCount: 0
-        });
-        newUserRequests[newUser] = newRequest;
-        emit NewRequestEvent(msg.sender, newUser, roles);
+        _userRequest[_from][_to][_newrole] = true;
+        emit NewRequestEvent(_from, _to, _newrole);
     }
 
-    function approveUserRequest(address newUser, uint8[] memory roles) external onlySuperAdmin {
-
-        NewUserRequest storage request =  newUserRequests[newUser];
-
-        for(uint8 i = 0; i < roles.length; i++)
+    // To approve user request, approver should be an admin and should vote only once
+    // This function will be called by different admins/approvers, so the vote info
+    // should be in storage
+    function approveUserRequest(address _from, address _to, uint8 _role)
+        external
+        onlyApprover
+        notAlreadyApproved(_from, _to, _role)
         {
-            uint8 role = roles[i];
-            if(approverVotes[newUser].voter[msg.sender][role])
-            {
-                revert("You have approved already");
-            } else {
-                approverVotes[newUser].voter[msg.sender][role] = true;
-                emit VoteUpdateEvent(msg.sender, newUser);
-            }
+
+        if(_isVoted[_from][_to][_role][msg.sender])
+        {
+            revert("VotedAlready");
+        } else {
+            _isVoted[_from][_to][_role][msg.sender] = true;
+            emit VoteUpdateEvent(_from, _to, _role, msg.sender);
         }
 
-        if(request.submitter == address(0))
-        {
-            revert("There is no valid request for given user");
-        }
+        _approvalCount[_from][_to][_role] ++;
 
-        newUserRequests[newUser].approvalCount++;
-        if (newUserRequests[newUser].approvalCount >= _approvalThreshold) {
-            uint8[] memory uroles = newUserRequests[newUser].roles;
-            for(uint8 i = 0; i < uroles.length; i++)
-            {
-                uint8 role = uroles[i];
-                _roles[role].members[newUser] = UserRoleData(UserStatus
-                .Active, true);
-                emit NewUserEvent(newUser, role);
-            }
-
-            delete newUserRequests[newUser];
-            delete approverVotes[newUser];
+        if (_approvalCount[_from][_to][_role] > _approvalThreshold) {
+            setPermissionBit(_from, _to, UserAction(_role));
+            emit NewApproveEvent(_from, _to, _role);
+            delete _userRequest[_from][_to][_role];
         }
     }
 
-    function disableUser(address oldUser, uint8 role)external
-        onlySuperAdmin
-        validRole(role) {
-
-        if(_roles[role].members[oldUser].status != UserStatus.Active)
-         {
-             revert("Invalid User State");
-         }
-
-        _roles[role].members[oldUser].isActive = false;
-        _roles[role].members[oldUser].status = UserStatus.Inactive;
-        emit UserStatusEvent(msg.sender, oldUser, UserStatus.Inactive);
+    // Check user action (READ/WRITE/DEPLOY/APPROVE)
+    function checkUserAction(address from, address to, UserAction action) external view returns(bool) {
+        return getPermissionBit(from, to, action);
     }
 
-    function enableUser(address oldUser, uint8 role)external
-        onlySuperAdmin
-        validRole(role) {
-         if(_roles[role].members[oldUser].status != UserStatus.Inactive)
-         {
-             revert("Invalid User State");
-         }
-
-        _roles[role].members[oldUser].isActive = true;
-        _roles[role].members[oldUser].status = UserStatus.Active;
-        emit UserStatusEvent(msg.sender, oldUser, UserStatus.Active);
-    }
-
-    function isWriter() external view returns (bool) {
-        return _roles[WRITER_ROLE].members[msg.sender].isActive;
-    }
-
-    function isViewer() external view returns (bool) {
-        return _roles[READER_ROLE].members[msg.sender].isActive;
-    }
-
-    function isSuperAdmin() external view returns(bool) {
-        return _roles[SUPER_ADMIN_ROLE].members[msg.sender].isActive;
-    }
-
-    function isDeployer() external view returns(bool) {
-        return _roles[DEPLOYER_ROLE].members[msg.sender].isActive;
-    }
-
-    function checkUserAction(address from, UserAction action) external view returns(bool) {
-        require (action == UserAction.Read || action == UserAction.Write || action == UserAction.Deploy, "Invalid Action");
-
-        if(action == UserAction.Deploy)
-        {
-            return _roles[DEPLOYER_ROLE].members[from].isActive;
-        } else if(action == UserAction.Write)
-        {
-            return _roles[WRITER_ROLE].members[from].isActive;
-        }
-        return _roles[READER_ROLE].members[from].isActive;
-    }
 }
