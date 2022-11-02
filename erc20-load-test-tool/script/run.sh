037b1c2e69b9b78c7f9e29790e5089b38af50851
Nischal Sharma <snischal@vmware.com>
2022-11-02 10:30:58 +0530

    Delete ERC20 load test tool
    Moved to the private repo - https://gitlab.eng.vmware.com/blockchain/vmwathena_blockchain/-/tree/master/vmbc-eth-sdk/tools/erc20-load-test-tool

--- erc20-load-test-tool/script/run.sh
+++
@@ -1,9 +0,0 @@
-version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
-echo version "$version"
-if [[ "$version" == "11"* ]]; then
-    echo Java version is 11
-    source .env
-    nohup python3 run-dapp.py &
-else
-    echo Java version is not 11
-fi
