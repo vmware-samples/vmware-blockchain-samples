85b06230fe29066d91f9fe89aca3d8b55c239c54
Bala Siva Sai Akhil Malepati <saiakhil2012@yahoo.com>
2022-11-02 03:24:38 -0700

    Handle success modal after a failure modal and omitting Stack from Error modal Signed-off-by: Bala Siva Sai Akhil Malepati <saiakhil2012@yahoo.com>

--- artemis/clarity/src/app/routes/digital-arts/digital-arts.component.ts
+++ artemis/clarity/src/app/routes/digital-arts/digital-arts.component.ts
@@ -140,7 +140,7 @@ export class DigitalArtsComponent implements OnInit, OnDestroy {
 
         //success message
         this.mintingResultModalMessage = 'Transaction successfully processed';
-        //this.mintingResultHasError = false;
+        this.mintingResultHasError = false;
         this.mintingResultModalData = JSON.stringify(result, null, this.mintingMessageJsonIndentSpaces);
         this.mintingResultModalShown = true;
 
