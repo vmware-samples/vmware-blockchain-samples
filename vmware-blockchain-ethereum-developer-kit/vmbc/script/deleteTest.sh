847c1da74155531f9a87efaaa088067b28f0b63f
Bala Siva Sai Akhil Malepati <saiakhil2012@yahoo.com>
2022-11-28 15:39:56 -0800

    Clean up previous dev kit content

--- vmware-blockchain-ethereum-developer-kit/vmbc/script/deleteTest.sh
+++
@@ -1,3 +0,0 @@
-kubectl delete -f ./runTest.yml -n vmbc-client1
-kubectl delete cm erc20-test-configmap -n vmbc-client1
-
--- vmware-blockchain-ethereum-developer-kit/vmbc/script/runTest.sh
+++
@@ -1,2 +0,0 @@
-kubectl create cm erc20-test-configmap --from-file=./testing --namespace=vmbc-client1
-kubectl apply -f ./runTest.yml --namespace=vmbc-client1
